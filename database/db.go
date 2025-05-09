86ed53061bb5bc6a175dddc4786d5710db454c40
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-09 17:21:47 -0300

    fix: refactor database connection to use environment variables

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,8 +14,18 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	os.Getenv("DB_HOST")
+	os.Getenv("DB_USER")
+	os.Getenv("DB_PASSWORD")
+
+	stringDeConexao := "host=" + os.Getenv("DB_HOST") + 
+		" user=" + os.Getenv("DB_USER") + 
+		" password=" + os.Getenv("DB_PASSWORD") + 
+		" dbname=" + os.Getenv("DB_NAME") + 
+		" port=" + os.Getenv("DB_PORT") + 
+		" sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
